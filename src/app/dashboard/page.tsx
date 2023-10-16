0b39a7612eee19ae338ecd0bb0cd3b67eee576cd
Abhinai Sai <abhinaisai2002>
2023-10-17 00:14:27 +0530

    fix: checked when user is logged or not and then redirected

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation';
 import React from 'react'
@@ -8,6 +9,15 @@ export default async function page() {
 
   if (!user || !user.id)
     redirect('/auth-callback?origin=dashboard')
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
+
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
 
   return (
     <div>{user.email}</div>
