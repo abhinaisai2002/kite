b2e61c1e8f0e30a8ce549fa159657363617d00b6
Abhinai Sai <abhinaisai2002>
2023-10-17 00:04:53 +0530

    feat: authcallback url setup completed

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { trpc } from '../_trpc/client';
+import { Loader2 } from 'lucide-react';
 
 export default function page() {
 
@@ -17,11 +18,24 @@ export default function page() {
       if (success) {
         router.push(`/${origin?origin:'dashboard'}`);
       }
-    }
+    },
+    onError: (err) => {
+      if (err.data?.code === 'UNAUTHORIZED') {
+        router.push('/sign-in')
+      }
+    },
   });
   
   return (
-    <div></div>
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>
+          Setting up your account...
+        </h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
   )
 }
 
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,15 +2,31 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCClientError } from '@trpc/client';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
  
 export const appRouter = router({
-  // ...
-  authCallback: publicProcedure.query(() => {
+
+  authCallback: publicProcedure.query(async () => {
     
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user || !user.id) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id:user.id
+      }
+    });
+
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email as string,
+        }
+      })
+    }
 
     return { success: true };
 
