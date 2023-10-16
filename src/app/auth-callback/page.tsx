411417a5b49e2b10cb98da2c22ee4c19b8ad0bbb
Abhinai Sai <abhinaisai2002>
2023-10-16 23:09:38 +0530

    feat: added env file with kinde api keys

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+export default function page() {
+
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const origin = searchParams.get('origin');
+  
+  return (
+    <div></div>
+  )
+}
