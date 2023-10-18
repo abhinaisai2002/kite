28f2f59aaf39d4732a31b1e569facc3e509c76b2
Abhinai Sai <abhinaisai2002>
2023-10-18 21:58:25 +0530

    feat: added functionalities for the pdf viewer
    - nextPage
    - previousPage
    - enter page number in the input

--- src/components/PDFRenderer.tsx
+++ src/components/PDFRenderer.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import { Loader2 } from 'lucide-react';
-import { PropsWithChildren } from 'react';
+import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
+import React, { PropsWithChildren } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css';
 import { useToast } from './ui/use-toast';
+
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod'
+import { cn } from '@/lib/utils';
+
 
 interface PDFPageProps extends PropsWithChildren{
   url: string
@@ -18,16 +27,93 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const PDFRenderer = ({ url }: PDFPageProps) => {
 
   const { toast } = useToast();
+
+
+  const [numPages, setNumPages] = React.useState<number>();
+  const [currPage, setCurrPage] = React.useState<number>(1)
+
+  const { width, ref } = useResizeDetector();
   
-  console.log(url);
+  const CustomPageValidator = z.object({
+    page: z
+      .string()
+      .refine(
+        (num) => Number(num) > 0 && Number(num) <= numPages!
+      ),
+  }) 
+  
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
+    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TCustomPageValidator>({
+    defaultValues: {
+      page:'1'
+    },
+    resolver:zodResolver(CustomPageValidator)
+  });
+
+  const handlePageSubmit = ({
+    page,
+  }: TCustomPageValidator) => {
+    setCurrPage(Number(page))
+    setValue('page', String(page))
+  }
+
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
       <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-        <div className='flex items-center gap-1.5'>topbar</div>
+        <div className='flex items-center gap-1.5'>
+          <Button
+            // disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) =>
+                prev - 1 > 1 ? prev - 1 : 1
+              )
+            }}
+            variant='ghost'
+            aria-label='previous page'>
+            <ChevronDown className='h-4 w-4' />
+          </Button>
+
+          <div className='flex items-center gap-1.5'>
+            <Input
+              {...register('page')}
+              className={cn(
+                'w-12 h-8',
+                errors.page && 'focus-visible:ring-red-500'
+              )}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSubmit(handlePageSubmit)()
+                }
+              }}
+            
+            />
+            <p className='text-zinc-700 text-sm space-x-1'>
+              <span>/</span>
+              <span>{numPages ?? 'x'}</span>
+            </p>
+          </div>
+
+          <Button
+            disabled={currPage === numPages || numPages === undefined}
+            onClick={() => {
+              setCurrPage((prev) => prev + 1 <= numPages! ? prev +1  : numPages!)
+            }}
+            variant='ghost'
+            aria-label='next page'>
+            <ChevronUp className='h-4 w-4' />
+          </Button>
+
+        </div>
       </div>
 
       <div className='flex-1 w-full max-h-screen'>
-        <div className='flex flex-row justify-center'>
+        <div className='flex flex-row justify-center' ref={ref}>
           <Document
             file={url}
             className='max-h-full'
@@ -43,8 +129,11 @@ const PDFRenderer = ({ url }: PDFPageProps) => {
                 variant: 'destructive',
               })
             }}
+            onLoadSuccess={({numPages}) => {
+              setNumPages(numPages);
+            }}
           >
-            <Page pageNumber={1} />
+            <Page width={width?width:1} pageNumber={currPage} />
           </Document>
         </div>
       </div>
