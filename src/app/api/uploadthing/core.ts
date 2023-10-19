e6aac63e8f040b6a79aa6e47ab1ebb6816a0ab33
Abhinai Sai <abhinaisai2002>
2023-10-19 21:36:26 +0530

    feat: completed inserting vectors in the db after uploading

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,12 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
+import { pinecone } from "@/lib/pinecone";
  
 const f = createUploadthing();
  
@@ -29,7 +35,53 @@ export const ourFileRouter = {
         },
       })
 
-      
+      try {
+        const response = await fetch(`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`);
+        const blob = await response.blob();
+
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+
+        const pineconeIndex = pinecone.Index('kite-dev');
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey:process.env.OPENAI_API_KEY
+        });
+        
+        await PineconeStore.fromDocuments(
+          pageLevelDocs,
+          embeddings,
+          {
+            // @ts-ignore
+            pineconeIndex:pineconeIndex,
+            namespace: createdFile.id,
+          }
+        )
+
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        })
+
+      }
+      catch (err) {
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        })
+      }
+
     }),
 } satisfies FileRouter;
  
