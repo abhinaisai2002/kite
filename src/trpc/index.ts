c8751ed8da8d52b851e7af25085976f19a498076
Abhinai Sai <abhinaisai2002>
2023-10-20 14:32:20 +0530

    feat: added payment integration with stripe
    - added checkout stripe page
    - added webhook for stripe
    - added api keys for stripe in .env

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -4,8 +4,13 @@ import { TRPCClientError } from '@trpc/client';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
 import { z } from 'zod';
-import { fi } from 'date-fns/locale';
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
+import { absoluteUrl } from '@/lib/utils';
+import {
+  getUserSubscriptionPlan,
+  stripe,
+} from '@/lib/stripe'
+import { PLANS } from '@/config/stripe';
  
 export const appRouter = router({
 
@@ -151,6 +156,63 @@ export const appRouter = router({
         nextCursor,
       }
     }),
+  createStripeSession: privateProcedure.mutation(
+    async ({ ctx }) => {
+      const { userId } = ctx
+
+      const billingUrl = absoluteUrl('/dashboard/billing')
+
+      if (!userId)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+      const dbUser = await db.user.findFirst({
+        where: {
+          id: userId,
+        },
+      })
+
+      if (!dbUser)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+      const subscriptionPlan =
+        await getUserSubscriptionPlan()
+
+      if (
+        subscriptionPlan.isSubscribed &&
+        dbUser.stripeCustomerId
+      ) {
+        const stripeSession =
+          await stripe.billingPortal.sessions.create({
+            customer: dbUser.stripeCustomerId,
+            return_url: billingUrl,
+          })
+
+        return { url: stripeSession.url }
+      }
+
+      const stripeSession =
+        await stripe.checkout.sessions.create({
+          success_url: billingUrl,
+          cancel_url: billingUrl,
+          payment_method_types: ['card',],
+          mode: 'subscription',
+          billing_address_collection: 'auto',
+          line_items: [
+            {
+              price: PLANS.find(
+                (plan) => plan.name === 'Pro'
+              )?.price.priceIds.test,
+              quantity: 1,
+            },
+          ],
+          metadata: {
+            userId: userId,
+          },
+        })
+
+      return { url: stripeSession.url }
+    }
+  ),
 });
  
 
